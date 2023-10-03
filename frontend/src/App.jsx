02c49966fe08c2944d7df46d64a4e3a19bf29f88
Sankar Battula <sankar.battula2@gmail.com>
2023-10-03 20:59:09 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+
 const App = () => {
   return (
     <>
-      <h1>MERN Auth</h1>
+      <Header />
+      <ToastContainer />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
